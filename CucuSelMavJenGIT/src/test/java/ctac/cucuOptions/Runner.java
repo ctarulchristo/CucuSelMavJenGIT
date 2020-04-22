9bf9cf2820335caa75c63bc00d8118f8d7e768bf
ctarulchristo <ctarulchristo1983@yahoo.com>
2020-04-22 14:20:08 +0530

    Updated to invoke cucumber test methods via testng/maven

--- CucuSelMavJenGIT/src/test/java/ctac/cucuOptions/Runner.java
+++ CucuSelMavJenGIT/src/test/java/ctac/cucuOptions/Runner.java
@@ -3,6 +3,7 @@ package ctac.cucuOptions;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -12,7 +13,7 @@ import cucumber.api.junit.Cucumber;
 			strict=true,
 			//dryRun=true,
 			plugin= {"pretty","html:target/cucumber","json:target/cucumber.json","junit:target/cukes.xml"})
-public class Runner {
-	
+public class Runner extends AbstractTestNGCucumberTests{
+	//to call needed methods from testgn when triggered via maven
 
 }
