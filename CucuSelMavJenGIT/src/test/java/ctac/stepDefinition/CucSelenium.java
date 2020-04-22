fc70d2db9d307b437e02415dda1b7ebbd4eb4b09
ctarulchristo <ctarulchristo1983@yahoo.com>
2020-04-22 18:52:51 +0530

    Update CucSelenium.java

--- CucuSelMavJenGIT/src/test/java/ctac/stepDefinition/CucSelenium.java
+++ CucuSelMavJenGIT/src/test/java/ctac/stepDefinition/CucSelenium.java
@@ -21,7 +21,7 @@ public class CucSelenium {
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
 	}
 
 	@Given("^User enters URL$")
